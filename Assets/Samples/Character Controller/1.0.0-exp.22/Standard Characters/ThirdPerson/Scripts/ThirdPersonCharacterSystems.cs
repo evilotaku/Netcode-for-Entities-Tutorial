e8923283a415ec20ecd6d21f0a21af66116fe4f6
evilotaku@gmail.com <>
2023-08-05 08:08:10 -0500

    DOTs character Controller: Physics

---
+++ Assets/Samples/Character Controller/1.0.0-exp.22/Standard Characters/ThirdPerson/Scripts/ThirdPersonCharacterSystems.cs
@@ -0,0 +1,181 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+using Unity.CharacterController;
+using Unity.Burst.Intrinsics;
+using Unity.Physics.Systems;
+
+[UpdateInGroup(typeof(KinematicCharacterPhysicsUpdateGroup))]
+[BurstCompile]
+public partial struct ThirdPersonCharacterPhysicsUpdateSystem : ISystem
+{
+    private EntityQuery _characterQuery;
+    private ThirdPersonCharacterUpdateContext _context;
+    private KinematicCharacterUpdateContext _baseContext;
+
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        _characterQuery = KinematicCharacterUtilities.GetBaseCharacterQueryBuilder()
+            .WithAll<
+                ThirdPersonCharacterComponent,
+                ThirdPersonCharacterControl>()
+            .Build(ref state);
+
+        _context = new ThirdPersonCharacterUpdateContext();
+        _context.OnSystemCreate(ref state);
+        _baseContext = new KinematicCharacterUpdateContext();
+        _baseContext.OnSystemCreate(ref state);
+
+        state.RequireForUpdate(_characterQuery);
+        state.RequireForUpdate<PhysicsWorldSingleton>();
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    { }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        _context.OnSystemUpdate(ref state);
+        _baseContext.OnSystemUpdate(ref state, SystemAPI.Time, SystemAPI.GetSingleton<PhysicsWorldSingleton>());
+
+        ThirdPersonCharacterPhysicsUpdateJob job = new ThirdPersonCharacterPhysicsUpdateJob
+        {
+            Context = _context,
+            BaseContext = _baseContext,
+        };
+        job.ScheduleParallel();
+    }
+
+    [BurstCompile]
+    [WithAll(typeof(Simulate))]
+    public partial struct ThirdPersonCharacterPhysicsUpdateJob : IJobEntity, IJobEntityChunkBeginEnd
+    {
+        public ThirdPersonCharacterUpdateContext Context;
+        public KinematicCharacterUpdateContext BaseContext;
+
+        void Execute(ThirdPersonCharacterAspect characterAspect)
+        {
+            characterAspect.PhysicsUpdate(ref Context, ref BaseContext);
+        }
+
+        public bool OnChunkBegin(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
+        {
+            BaseContext.EnsureCreationOfTmpCollections();
+            return true;
+        }
+
+        public void OnChunkEnd(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask, bool chunkWasExecuted)
+        { }
+    }
+}
+
+[UpdateInGroup(typeof(KinematicCharacterVariableUpdateGroup))]
+[BurstCompile]
+public partial struct ThirdPersonCharacterVariableUpdateSystem : ISystem
+{
+    private EntityQuery _characterQuery;
+    private ThirdPersonCharacterUpdateContext _context;
+    private KinematicCharacterUpdateContext _baseContext;
+
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        _characterQuery = KinematicCharacterUtilities.GetBaseCharacterQueryBuilder()
+            .WithAll<
+                ThirdPersonCharacterComponent,
+                ThirdPersonCharacterControl>()
+            .Build(ref state);
+
+        _context = new ThirdPersonCharacterUpdateContext();
+        _context.OnSystemCreate(ref state);
+        _baseContext = new KinematicCharacterUpdateContext();
+        _baseContext.OnSystemCreate(ref state);
+
+        state.RequireForUpdate(_characterQuery);
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    { }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        _context.OnSystemUpdate(ref state);
+        _baseContext.OnSystemUpdate(ref state, SystemAPI.Time, SystemAPI.GetSingleton<PhysicsWorldSingleton>());
+
+        ThirdPersonCharacterVariableUpdateJob job = new ThirdPersonCharacterVariableUpdateJob
+        {
+            Context = _context,
+            BaseContext = _baseContext,
+        };
+        job.ScheduleParallel();
+    }
+
+    [BurstCompile]
+    [WithAll(typeof(Simulate))]
+    public partial struct ThirdPersonCharacterVariableUpdateJob : IJobEntity, IJobEntityChunkBeginEnd
+    {
+        public ThirdPersonCharacterUpdateContext Context;
+        public KinematicCharacterUpdateContext BaseContext;
+
+        void Execute(ThirdPersonCharacterAspect characterAspect)
+        {
+            characterAspect.VariableUpdate(ref Context, ref BaseContext);
+        }
+
+        public bool OnChunkBegin(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
+        {
+            BaseContext.EnsureCreationOfTmpCollections();
+            return true;
+        }
+
+        public void OnChunkEnd(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask, bool chunkWasExecuted)
+        { }
+    }
+}
+
+[UpdateInGroup(typeof(AfterPhysicsSystemGroup))]
+[UpdateAfter(typeof(KinematicCharacterPhysicsUpdateGroup))]
+public partial class CharacterHitsDetectionSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        // Iterate on non-stateful hits
+        foreach (var hitsBuffer in SystemAPI.Query<DynamicBuffer<KinematicCharacterHit>>())
+        {
+            for (int i = 0; i < hitsBuffer.Length; i++)
+            {
+                KinematicCharacterHit hit = hitsBuffer[i];
+                if (!hit.IsGroundedOnHit)
+                {
+                    UnityEngine.Debug.Log($"Detected an ungrounded hit {hit.Entity.Index}");
+                }
+            }
+        }
+
+        // Iterate on stateful hits
+        foreach (var statefulHitsBuffer in SystemAPI.Query<DynamicBuffer<StatefulKinematicCharacterHit>>())
+        {
+            for (int i = 0; i < statefulHitsBuffer.Length; i++)
+            {
+                StatefulKinematicCharacterHit hit = statefulHitsBuffer[i];
+                if (hit.State == CharacterHitState.Enter)
+                {
+                    UnityEngine.Debug.Log($"Entered new hit {hit.Hit.Entity.Index}");
+                }
+                else if (hit.State == CharacterHitState.Exit)
+                {
+                    UnityEngine.Debug.Log($"Exited a hit {hit.Hit.Entity.Index}");
+                }
+            }
+        }
+    }
+}
